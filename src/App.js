9575e01973202caad8cf0292cde3c007cdae1df5
wish9919 <vishvantha321@gmail.com>
2020-05-12 19:55:05 +0530

    auth screen update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/Signup";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </Router>
