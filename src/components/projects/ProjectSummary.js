2ee716258f837bfc182b3ef69c2d073adc8c1e88
wish9919 <vishvantha321@gmail.com>
2020-06-07 02:23:05 +0530

    moment added

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 const ProjectSummary = ({ project }) => {
   // console.log(project);
@@ -9,7 +10,9 @@ const ProjectSummary = ({ project }) => {
         <p>
           Posted by {`${project.authorFirstName} ${project.authorLastName}`}
         </p>
-        <p className="grey-text">{project.createdAt}</p>
+        <p className="grey-text">
+          {moment(project.createdAt.toDate()).calendar()}
+        </p>
       </div>
     </div>
   );
--- src/store/actions/ProjectActions.js
+++ src/store/actions/ProjectActions.js
@@ -6,11 +6,6 @@ export const createProject = (project) => {
     const profile = getState().firebase.profile;
     const authorId = getState().firebase.auth.uid;
 
-    const d = new Date();
-    const time = new Date().toLocaleTimeString();
-    const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-    const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-    const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
     firestore
       .collection("projects")
       .add({
@@ -18,7 +13,7 @@ export const createProject = (project) => {
         authorFirstName: profile.firstName,
         authorLastName: profile.lastName,
         authorId: authorId,
-        createdAt: `${mo} ${da} ${ye}, ${time}`,
+        createdAt: new Date(),
       })
       .then(() => {
         dispatch({ type: "CREATE_PROJECT", project });
