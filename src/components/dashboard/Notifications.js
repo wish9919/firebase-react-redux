7da1d091b4ddfb5e6077873b5cdd5d47c22b362f
wish9919 <vishvantha321@gmail.com>
2020-06-08 00:13:32 +0530

    update notifications

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -3,6 +3,9 @@ import moment from "moment";
 
 const Notifications = (props) => {
   const { notifications, user } = props;
+  const userNotifications =
+    notifications && notifications.filter((item) => item.userId === user);
+
   return (
     <div className="section">
       <div className="card z-depth-0">
@@ -10,25 +13,27 @@ const Notifications = (props) => {
           <span className="card-title">Notifications</span>
 
           <ul className="notifications">
-            {notifications &&
-              notifications
-                .filter((item) => item.userId === user)
-                .map((item) => {
-                  return (
-                    <li style={{ marginBottom: 10 }} key={item.id}>
-                      <div>{item.content}</div>
-                      <span style={{ fontSize: 12 }} className="pink-text">
-                        {item.user}{" "}
-                      </span>
-                      <span
-                        style={{ fontSize: 12 }}
-                        className="grey-text note-date"
-                      >
-                        {moment(item.time.toDate()).fromNow()}
-                      </span>
-                    </li>
-                  );
-                })}
+            {}
+            {userNotifications && userNotifications.length !== 0 ? (
+              userNotifications.map((item) => {
+                return (
+                  <li style={{ marginBottom: 10 }} key={item.id}>
+                    <div>{item.content}</div>
+                    <span style={{ fontSize: 12 }} className="pink-text">
+                      {item.user}{" "}
+                    </span>
+                    <span
+                      style={{ fontSize: 12 }}
+                      className="grey-text note-date"
+                    >
+                      {moment(item.time.toDate()).fromNow()}
+                    </span>
+                  </li>
+                );
+              })
+            ) : (
+              <li className="grey-text">Don't have any Notifications</li>
+            )}
           </ul>
         </div>
       </div>
